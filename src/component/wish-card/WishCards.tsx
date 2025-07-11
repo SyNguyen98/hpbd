2bd5a1ddc1fd464a5e4632c1a964adc29f64c346
synguyen <nghsynguyen98@gmail.com>
2025-07-11 23:05:08 +0700

    feat: update WishCards component with new background colors and enhanced messages

--- src/component/wish-card/WishCards.tsx
+++ src/component/wish-card/WishCards.tsx
@@ -3,13 +3,13 @@ import "./WishCards.scss"
 function WishCards() {
     return (
         <div className="wish-cards">
-            <div className="wish-card">
+            <div className="wish-card" style={{backgroundColor: '#E3F2FD'}}>
                 <img src='https://cdn.jsdelivr.net/gh/SyNguyen98/image-storage@main/hpbd/wish-1.webp?raw=true'
                      alt="Duong & Minh Anh"
                      loading="lazy"/>
                 <div className="wish-content">
                     <p className="content">
-                        Thêm một năm đã qua, heo hai đã thêm 1 tuổi chúc hai sự nghiệp thành đạt như thuyền gặp gió ⛵️, tiền vô như nước :)))
+                        Thêm một năm đã qua, heo hai đã thêm 1 tuổi. Chúc hai sự nghiệp thành đạt như thuyền gặp gió ⛵️, tiền vô như nước :)))
                         có j mua bánh tráng cho em càng tốt 😋, mau hết què chân, dồi dào sức khỏe 💪
                     </p>
                     <p className="sign">
@@ -18,13 +18,18 @@ function WishCards() {
                 </div>
             </div>
 
-            <div className="wish-card">
+            <div className="wish-card" style={{backgroundColor: '#E8F5E9'}}>
                 <img src='https://cdn.jsdelivr.net/gh/SyNguyen98/image-storage@main/hpbd/wish-2.webp?raw=true'
                      alt="An & Minh Anh"
                      loading="lazy"/>
                 <div className="wish-content">
                     <p className="content">
-                        Thêm ...
+                        Hé lô người đẹp, cuối cùng tui cũng có thể sống đến ngày có thể nhìn Minh Anh có người yêu và
+                        viết lời chúc này vì người yêu MinAnh muốn tạo bất ngờ =))
+                    </p>
+                    <p className="content">
+                        Này chắc lần thứ 9 chúc mừng sn bà rồi nhỉ, chúc bà tuổi mới hạnh phúc, vui vẻ, ngày càng tài giỏi
+                        và vẫn giữ được sự vô tri hài hước, tuổi mới rực rỡ hơn nhá
                     </p>
                     <p className="sign">
                         - Ân -
@@ -32,7 +37,7 @@ function WishCards() {
                 </div>
             </div>
 
-            <div className="wish-card">
+            <div className="wish-card" style={{backgroundColor: '#FCE4EC'}}>
                 <img src='https://cdn.jsdelivr.net/gh/SyNguyen98/image-storage@main/hpbd/wish-3.webp?raw=true'
                      alt="we"
                      loading="lazy"/>
